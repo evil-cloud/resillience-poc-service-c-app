28400c6925b74f7127f5c790af3421660962ef65
Jose Reynoso <reynosojose2005@gmail.com>
2025-02-21 17:34:28 -0600

    comment: vie 21 feb 2025 17:34:28 CST

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import logging
+from prometheus_fastapi_instrumentator import Instrumentator
+
+# Configurar logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+
+# Instrumentar Prometheus
+Instrumentator().instrument(app).expose(app)
+
+@app.get("/")
+async def always_ok():
+    logger.info("Service C respondió exitosamente (fallback)")
+    return {"message": "Hola desde C, soy el backup de B"}
+
